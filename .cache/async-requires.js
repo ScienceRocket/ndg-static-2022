5f4a8f660ead3025b5954da1d46c4983bec3a217
ScienceJ <langenhovenh@yahoo.com>
2022-06-16 18:07:59 +0200

    Clean

---
+++ .cache/async-requires.js
@@ -0,0 +1,5 @@
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-index-jsx": () => import("./../../../src/pages/index.jsx" /* webpackChunkName: "component---src-pages-index-jsx" */)
+}
+
--- src/pages/index.jsx
+++
@@ -1,7 +0,0 @@
-import { Script } from "gatsby"
-import React from "react"
-export default function Home(){
-    window.location.replace('https://play.google.com/store/apps/dev?id=9120765401739126892');
-    return null;
-
-}
