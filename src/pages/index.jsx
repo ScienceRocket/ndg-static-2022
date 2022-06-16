357583b3709f57be3bf1906bd9d66cd5aac6af66
ScienceJ <langenhovenh@yahoo.com>
2022-06-16 09:44:07 +0200

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,7 @@
+import { Script } from "gatsby"
 import React from "react"
 export default function Home(){
+    window.location.replace('https://play.google.com/store/apps/dev?id=9120765401739126892');
     return <div>Hello</div>
 
 }
